1d5001216b72bbdd4aefcc0cde5f4e1015944611
koizachek <85067527+koizachek@users.noreply.github.com>
2025-01-13 18:19:21 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -84,9 +84,9 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("Chat with any PDF")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
+    user_question = st.text_input("Ask the PDF File/s a Question")
 
     if user_question:
         user_input(user_question)
